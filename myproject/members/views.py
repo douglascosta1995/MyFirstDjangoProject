330c1a7a02736daae04d0ba0e4f9cba5ec7ef0ae
Douglas <douglascastro2010@hotmail.com>
2022-10-07 22:36:48 +0200

    Initial setup created for practicing Django WebApplication development

---
+++ myproject/members/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+
+from .models import Members
+
+
+def index(request):
+    my_members = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'my_members': my_members
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template("add.html")
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
